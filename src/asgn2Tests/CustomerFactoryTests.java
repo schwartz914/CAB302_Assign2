931a3ba9a3b59fe89cb172669dfced97f9dd6938
Brodie Birko <brodie.birkett@connect.qut.edu.au>
2017-05-05 00:24:29 -0700

    Added test outlines (in comments) for CustomerFactoryTests, CustomerTests, LogHandlerCustomerTests and RestaurantCustomerTests classes.

--- src/asgn2Tests/CustomerFactoryTests.java
+++ src/asgn2Tests/CustomerFactoryTests.java
@@ -8,4 +8,10 @@ package asgn2Tests;
  */
 public class CustomerFactoryTests {
 	// TO DO
+	
+	// CustomerFactory.java Tests
+		// --- getCustomer() Tests	---
+		// CustomerException thrown if invalid customer code; not PUC, DNC or DVC
+		// Returns customer object
+		// Returns correct PickUpCustomer, DriverDeliveryCustomer or DroneDeliveryCustomer
 }
--- src/asgn2Tests/LogHandlerCustomerTests.java
+++ src/asgn2Tests/LogHandlerCustomerTests.java
@@ -7,4 +7,25 @@ package asgn2Tests;
  */
 public class LogHandlerCustomerTests {
 	// TO DO
+	
+	// LogHandler.java Tests
+		// --- populateCustomerDataset() Tests ---
+		// Test returns array list of customer objects from information log file
+		// Returns customers in same order as log file
+		// Throw CustomerException if contains semantic errors violate 5.3:
+		// Takes correct form (order-time, delivery-time, ...)
+		// If order-time & delivery time incorrect format hh:mm:ss
+		// Customer name doesn't contain a name
+		// customer-number doesn't contain a number
+		// Customer code incorrect
+		// X and y locations don't represent blocks from restaurant
+		// Throw LogHandlerException if problem with log file not related to above conditions 
+		// Fields not of correct type (8.1.1)
+		
+		// --- createCustomer() Tests ---
+		// Creates customer object from single line of log file
+		// Returns customer object
+		// Throws customer exception for same reason above (5.3)
+		// Throws logHandlerException if problem parsing the line from log file
+	
 }
