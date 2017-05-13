4274b9560261c6886a218839bea2d9dcec01bd47
Brodie Birko <brodie.birkett@connect.qut.edu.au>
2017-05-12 18:11:00 -0700

    Finished tests for RestaurantCustomerTests.java and added log files used for testing.

--- src/asgn2Tests/RestaurantCustomerTests.java
+++ src/asgn2Tests/RestaurantCustomerTests.java
@@ -44,16 +44,65 @@ public class RestaurantCustomerTests {
 		}
 		
 		// --- processLog() Tests ---
-		//Return true if file correctly processed
+		// Return true if file correctly processed
+		@Test
+		public void processedCorrectly() throws CustomerException, PizzaException, LogHandlerException {
+			assertTrue(restaurant2.processLog("20170101.txt"));
+		}
 		// Return false if not
+		// Throw LogHandlerException if problem with the log file not related to above
+		@Test(expected = LogHandlerException.class)
+		public void processedIncorrectly() throws CustomerException, PizzaException, LogHandlerException {
+			assertFalse(restaurant2.processLog("notRealFile.txt"));
+		}
 		// Throw CustomerException if contains semantic errors violate 5.3:
 		// Takes correct form (order-time, delivery-time, ...)
 		// If order-time & delivery time incorrect format hh:mm:ss
+		@Test(expected = CustomerException.class)
+		public void deliveryTimeCorrectForm() throws CustomerException, PizzaException, LogHandlerException {
+			assertTrue(restaurant2.processLog("delivery-timeWrongFormat.txt"));
+		}
+		@Test(expected = CustomerException.class)
+		public void orderTimeCorrectForm() throws CustomerException, PizzaException, LogHandlerException {
+			assertTrue(restaurant2.processLog("order-timeWrongFormat.txt"));
+		}
 		// Customer name doesn't contain a name
-		// customer-number doesn't contain a number
+		//		Not just white spaces
+		@Test(expected = CustomerException.class)
+		public void nameNotSpaces() throws CustomerException, PizzaException, LogHandlerException {
+			assertTrue(restaurant2.processLog("customer-nameWrongFormat1.txt"));
+		}
+		//		No numbers
+		@Test(expected = CustomerException.class)
+		public void nameNotNumber() throws CustomerException, PizzaException, LogHandlerException {
+			assertTrue(restaurant2.processLog("customer-nameWrongFormat2.txt"));
+		}
+		// customer-number doesn't contain numbers
+		//		Only Numbers
+		@Test(expected = CustomerException.class)
+		public void numberOnlyNumber() throws CustomerException, PizzaException, LogHandlerException {
+			assertTrue(restaurant2.processLog("customer-numberWrongFormat1.txt"));
+		}
+		//		No White spaces
+		@Test(expected = CustomerException.class)
+		public void numberNotSpaces() throws CustomerException, PizzaException, LogHandlerException {
+			assertTrue(restaurant2.processLog("customer-numberWrongFormat2.txt"));
+		}
 		// Customer code incorrect
-		// X and y locations don't represent blocks from restaurant
-		// Throw LogHandlerException if problem with the log file not related to above
+		@Test(expected = CustomerException.class)
+		public void incorrectCustomerCode() throws CustomerException, PizzaException, LogHandlerException {
+			assertTrue(restaurant2.processLog("customer-codeWrongFormat.txt"));
+		}
+		// x location don't represent blocks from restaurant
+		@Test(expected = CustomerException.class)
+		public void xLocationNotBlocks() throws CustomerException, PizzaException, LogHandlerException {
+			assertTrue(restaurant2.processLog("customer-x-locationWrongFormat1.txt"));
+		}
+		// y location don't represent blocks from restaurant
+		@Test(expected = CustomerException.class)
+		public void yLocationsNotBlocks() throws CustomerException, PizzaException, LogHandlerException {
+			assertTrue(restaurant2.processLog("customer-y-locationWrongFormat1.txt"));
+		}
 
 		// --- getCustomerByIndex() Tests ---
 		// Index the same as in the log file & Returns correct customer
@@ -67,7 +116,7 @@ public class RestaurantCustomerTests {
 		// Throws CustomerException if the index is invalid
 		@Test(expected = CustomerException.class)
 		public void indexInvalid() throws CustomerException, PizzaException, LogHandlerException {
-			restaurant2.processLog("20170101");
+			restaurant2.processLog("20170101.txt");
 			assertEquals(restaurant2.getCustomerByIndex(6), 0);
 		}
 		
@@ -75,13 +124,13 @@ public class RestaurantCustomerTests {
 		// Return the correct number of objects in customer field
 		@Test
 		public void numCustomerOrders() throws CustomerException, PizzaException, LogHandlerException {
-			restaurant2.processLog("20170101");
+			restaurant2.processLog("20170101.txt");
 			assertEquals(restaurant2.getNumCustomerOrders(), 3);
 		}
 		// Test same value as getNumPizzaOrders()
 		@Test
 		public void numPizzaCustomerSame() throws CustomerException, PizzaException, LogHandlerException {
-			restaurant2.processLog("20170101");
+			restaurant2.processLog("20170101.txt");
 			assertEquals(restaurant2.getNumCustomerOrders(), restaurant2.getNumPizzaOrders());
 		}
 	
@@ -89,7 +138,7 @@ public class RestaurantCustomerTests {
 		// Return correct total delivery distance for all customers
 		@Test
 		public void correctTotalDeliveryDistance() throws CustomerException, PizzaException, LogHandlerException {
-			restaurant2.processLog("20170101");
+			restaurant2.processLog("20170101.txt");
 			double expectedValue = restaurant2.getCustomerByIndex(0).getDeliveryDistance() + restaurant2.getCustomerByIndex(1).getDeliveryDistance() + restaurant2.getCustomerByIndex(2).getDeliveryDistance();
 			assertEquals(restaurant2.getTotalDeliveryDistance(), expectedValue, 0);
 		}
@@ -100,7 +149,7 @@ public class RestaurantCustomerTests {
 		@Test(expected = CustomerException.class)
 		public void resetDetails() throws CustomerException, PizzaException, LogHandlerException {
 			restaurant2.resetDetails();
-			restaurant2.processLog("20170101");
+			restaurant2.processLog("20170101.txt");
 			restaurant2.resetDetails();
 			assertEquals(restaurant2.getNumCustomerOrders(), 0);
 		}
